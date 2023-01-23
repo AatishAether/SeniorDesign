14c690c8d43e15375bf721421e85daa91a649fe0
dbeleiu <38052665+dbeleiu@users.noreply.github.com>
2023-01-23 00:43:12 -0500

    Add files via upload

--- CurrentScripts/main.py
+++ CurrentScripts/main.py
@@ -1,63 +1,19 @@
-import os
-import pandas as pd
 import torch
-import cv2
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
-import random
+import numpy as np
+import pandas as pd
+import cv2
+import os
+from torch.utils.data import DataLoader
+from torchvision.io import read_image
 
-
-train_set = r'.\tP\alph_train'
-test_set = r'.\tP\alph_test'
-
-def build_csv(directory_string, output_csv_name):
-    """Builds a csv file for pytorch training from a directory of folders of images.
-    Install csv module if not already installed.
-    Args:
-    directory_string: string of directory path, e.g. r'.\data\train'https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
-    output_csv_name: string of output csv file name, e.g. 'train.csv'
-    Returns:
-    csv file with file names, file paths, class names and class indices
-    """
-    import csv
-    directory = directory_string
-    class_lst = os.listdir(directory) #returns a LIST containing the names of the entries (folder names in this case) in the directory.
-    class_lst.sort() #IMPORTANT
-    with open(output_csv_name, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(['file_name', 'file_path', 'class_name', 'class_index']) #create column names
-        for class_name in class_lst:
-            class_path = os.path.join(directory, class_name) #concatenates various path components with exactly one directory separator (‘/’) except the last path component.
-            file_list = os.listdir(class_path) #get list of files in class folder
-            for file_name in file_list:
-                file_path = os.path.join(directory, class_name, file_name) #concatenate class folder dir, class name and file name
-                writer.writerow([file_name, file_path, class_name, class_lst.index(class_name)]) #write the file path and class name to the csv file
-    return
-
-build_csv(train_set, 'train.csv')
-build_csv(test_set, 'test.csv')
-train_df = pd.read_csv('train.csv')
-test_df = pd.read_csv('test.csv')
-
-class_zip = zip(train_df['class_index'], train_df['class_name'])
-my_list = []
-for index, name in class_zip:
-  tup = tuple((index, name))
-  my_list.append(tup)
-unique_list = list(set(my_list))
-print('Training:')
-print(sorted(unique_list))
-print()
-
-class_zip = zip(test_df['class_index'], test_df['class_name'])
-my_list = []
-for index, name in class_zip:
-  tup = tuple((index, name))
-  my_list.append(tup)
-unique_list = list(set(my_list))
-print('Testing:')
-print(sorted(unique_list))
-
-class_names = list(train_df['class_name'].unique())
+batch_size = 50
+num_workers = 8
 
 class ASLDataset(torch.utils.data.Dataset): # inheritin from Dataset class
     def __init__(self, csv_file, root_dir="", transform=None):
@@ -70,32 +26,181 @@ class ASLDataset(torch.utils.data.Dataset): # inheritin from Dataset class
 
     def __getitem__(self, idx):
         image_path = os.path.join(self.root_dir, self.annotation_df.iloc[idx, 1]) #use image path column (index = 1) in csv file
-        image = cv2.imread(image_path) # read image by cv2
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # convert from BGR to RGB for matplotlib
-        class_name = self.annotation_df.iloc[idx, 2] # use class name column (index = 2) in csv file
-        class_index = self.annotation_df.iloc[idx, 3] # use class index column (index = 3) in csv file
+        image = read_image(image_path)
+        label = self.annotation_df.iloc[idx, 3]
+        # image = cv2.imread(image_path) # read image by cv2
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # convert from BGR to RGB for matplotlib
+        # class_name = self.annotation_df.iloc[idx, 2] # use class name column (index = 2) in csv file
+        # class_index = self.annotation_df.iloc[idx, 3] # use class index column (index = 3) in csv file
         if self.transform:
             image = self.transform(image)
-        return image, class_name, class_index
+        return image, label #class_index , class_name
 
-#test dataset class without transformation:
-train_dataset_untransformed = ASLDataset(csv_file='train.csv', root_dir="", transform=None)
+train_dataset = ASLDataset('./train.csv') #, train_transform)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-#visualize 10 random images from the loaded dataset
-plt.figure(figsize=(12,12))
-for i in range(10):
-    idx = random.randint(0, len(train_dataset_untransformed))
-    image, class_name, class_index = train_dataset_untransformed[idx]
-    ax=plt.subplot(2,5,i+1) # create an axis
-    ax.title.set_text(class_name + '-' + str(class_index)) # create a name of the axis based on the img name
-    plt.imshow(image) # show the img
-plt.show()
+val_dataset = ASLDataset('./test.csv')  # val.csv
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-if torch.cuda.is_available():
-    print(
-        "yes"
-    )
-else:
-    print(
-        "no"
-    )
+classes = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'nothing', 'O', 'P', 'Q', 'R', 'S', 'space', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+
+# Define a convolution neural network
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=5, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(12)
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=5, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(12)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv4 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(24)
+        self.conv5 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=5, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(24)
+        self.fc1 = nn.Linear(24 * 10 * 10, 10)
+
+    def forward(self, input):
+        output = F.relu(self.bn1(self.conv1(input)))
+        output = F.relu(self.bn2(self.conv2(output)))
+        output = self.pool(output)
+        output = F.relu(self.bn4(self.conv4(output)))
+        output = F.relu(self.bn5(self.conv5(output)))
+        output = output.view(-1, 24 * 10 * 10)
+        output = self.fc1(output)
+
+        return output
+
+
+# Instantiate a neural network model
+model = Network()
+
+# Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+
+
+# Function to save the model
+def saveModel():
+    path = "./myFirstModel.pth"
+    torch.save(model.state_dict(), path)
+
+
+# Function to test the model with the test dataset and print the accuracy for the test images
+def testAccuracy():
+    model.eval()
+    accuracy = 0.0
+    total = 0.0
+
+    with torch.no_grad():
+        for data in val_dataloader:
+            images, labels = data
+            # run the model on the test set to predict labels
+            outputs = model(images)
+            # the label with the highest energy will be our prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            accuracy += (predicted == labels).sum().item()
+
+    # compute the accuracy over all test images
+    accuracy = (100 * accuracy / total)
+    return (accuracy)
+
+
+# Training function. We simply have to loop over our data iterator and feed the inputs to the network and optimize.
+def train(num_epochs):
+    best_accuracy = 0.0
+
+    # Define your execution device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("The model will be running on", device, "device")
+    # Convert model parameters and buffers to CPU or Cuda
+    model.to(device)
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        running_acc = 0.0
+
+        for i, (images, labels) in enumerate(train_dataloader, 0):
+
+            # get the inputs
+            images = Variable(images.to(device))
+            print(type(labels))
+            labels = Variable(labels.to(device))
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # predict classes using images from the training set
+            outputs = model(images)
+            # compute the loss based on model output and real labels
+            loss = loss_fn(outputs, labels)
+            # backpropagate the loss
+            loss.backward()
+            # adjust parameters based on the calculated gradients
+            optimizer.step()
+
+            # Let's print statistics for every 1,000 images
+            running_loss += loss.item()  # extract the loss value
+            if i % 1000 == 999:
+                # print every 1000 (twice per epoch)
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 1000))
+                # zero the loss
+                running_loss = 0.0
+
+        # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
+        accuracy = testAccuracy()
+        print('For epoch', epoch + 1, 'the test accuracy over the whole test set is %d %%' % (accuracy))
+
+        # we want to save the model if the accuracy is the best
+        if accuracy > best_accuracy:
+            saveModel()
+            best_accuracy = accuracy
+
+
+# Function to show the images
+def imageshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# Function to test the model with a batch of images and show the labels predictions
+def testBatch():
+    # get batch of images from the test DataLoader
+    images, labels = next(iter(val_dataloader))
+
+    # show all images as one image grid
+    imageshow(torchvision.utils.make_grid(images))
+
+    # Show the real labels on the screen
+    print('Real labels: ', ' '.join('%5s' % classes[labels[j]]
+                                    for j in range(batch_size)))
+
+    # Let's see what if the model identifiers the  labels of those example
+    outputs = model(images)
+
+    # We got the probability for every 10 labels. The highest (max) probability should be correct label
+    _, predicted = torch.max(outputs, 1)
+
+    # Let's show the predicted labels on the screen to compare with the real ones
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                  for j in range(batch_size)))
+
+
+if __name__ == "__main__":
+    # Let's build our model
+    train(5)
+    print('Finished Training')
+
+    # Test which classes performed well
+    testAccuracy()
+
+    # Let's load the model we just created and test the accuracy per label
+    model = Network()
+    path = "myFirstModel.pth"
+    model.load_state_dict(torch.load(path))
+
+    # Test with batch of images
+    testBatch()
